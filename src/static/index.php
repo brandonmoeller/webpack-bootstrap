c2949d4aabe1eb7a37fa0a950bf4941fa3979d43
Brandon Moeller <b@brandonmoeller.com>
2020-04-30 23:23:13 -0500

    Added an exclamation point! to the title

--- src/static/index.php
+++ src/static/index.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Bootstrap 4 Template - Hello, World</title>
+    <title>Bootstrap 4 Template - Hello, World!</title>
 
     <!-- Website icon -->
     <link rel="icon" type="image/png" href="http://placehold.it/20x20">
